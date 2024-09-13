98d74d0fdf9cc93a1811399909f83e68fe5ecd3e
Zaryab <zaryabkhan248@gmail.com>
2024-09-13 07:43:32 +0500

    installing some important libraries

--- controllers/removeBg.controller.js
+++ controllers/removeBg.controller.js
@@ -8,50 +8,58 @@ import fs from "fs";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 async function removeBg(req, res) {
-  if (!req.file) {
-    return res.status(404).json({
-      message: "Error occur while uploading file to server",
-    });
-  }
-
-  // Define file paths for input and output
-  const filePath = path.resolve(__dirname, "../public/", req.file.filename);
-  const outputPath = path.resolve(
-    __dirname,
-    "../public/",
-    `quickbgremove_${req.file.filename}.png`
-  );
-
-  // Verify the uploaded file exists
-  if (!fs.existsSync(filePath)) {
-    return res.status(404).json({
-      message: "Uploaded file not found",
-    });
-  }
-
-  // Command to run the Python script with proper path quoting
-  const command = `python "remove_background.py" "${filePath}" "${outputPath}"`;
-
-  // Execute the Python command
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      return res.status(500).send("Error occur while processing image");
+  try {
+    if (!req.file) {
+      return res.status(404).json({
+        message: "Error occur while uploading file to server",
+      });
     }
-    // Check if the output file exists before sending it back
-    if (!fs.existsSync(outputPath)) {
-      return res.status(404).send("Output file not found");
+
+    // Define file paths for input and output
+    const filePath = path.resolve(__dirname, "../public/", req.file.filename);
+    const outputPath = path.resolve(
+      __dirname,
+      "../public/",
+      `quickbgremove_${req.file.filename}.png`
+    );
+
+    // Verify the uploaded file exists
+    if (!fs.existsSync(filePath)) {
+      return res.status(404).json({
+        message: "Uploaded file not found",
+      });
     }
-    // Send the processed file as a response
-    res.sendFile(outputPath, (err) => {
-      if (err) {
-        console.error("Error occur while sending image back to you:");
-      } else {
-        // Optionally delete the file after sending
-        fs.unlinkSync(filePath);
-        fs.unlinkSync(outputPath);
+
+    // Command to run the Python script with proper path quoting
+    const command = `python "remove_background.py" "${filePath}" "${outputPath}"`;
+
+    // Execute the Python command
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        return res.status(500).send("Error occur while processing image");
       }
+      // Check if the output file exists before sending it back
+      if (!fs.existsSync(outputPath)) {
+        return res.status(404).send("Output file not found");
+      }
+      // Send the processed file as a response
+      res.sendFile(outputPath, (err) => {
+        if (err) {
+          res.status(404).send("Error occur while sending image back to you:");
+        } else {
+          // Optionally delete the file after sending
+          fs.unlinkSync(filePath);
+          fs.unlinkSync(outputPath);
+        }
+      });
     });
-  });
+  } catch (error) {
+    fs.unlinkSync(filePath);
+    fs.unlinkSync(outputPath);
+    return res
+      .status(501)
+      .send("Error occur while proccesing image please try again");
+  }
 }
 
 export { removeBg };
--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 import express from "express";
 import cors from "cors";
 import toolsOperationRouter from "./routes/toolsOperations.route.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import helmet from "helmet";
+
+dotenv.config();
 const app = express();
 
-// for accepting json format data
+// specifying client origins
+const allowedOrigins = [
+  "https://quickbgremove.netlify.app",
+  "http://localhost:5173",
+];
+
+// built in middlewares
+app.use(compression()); // Gzip compression
+app.use(helmet()); // Secure HTTP headers
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
 // api checker
 app.get("/", (req, res) => {
-  res.json("api is working");
+  res.json("QuickBgRemove Api is working");
 });
 
 // all routes
 app.use("/api/tools", toolsOperationRouter);
 
-app.listen(3000, () => {
-  console.log("app is listening");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on ${process.env.PORT} port `);
+  dbConnection();
 });
