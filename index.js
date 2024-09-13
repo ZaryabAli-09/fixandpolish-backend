3c64de80174ca466b1dbe6ef2d07e67fd5863da4
Zaryab <zaryabkhan248@gmail.com>
2024-09-13 10:02:45 +0500

    cc

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import toolsOperationRouter from "./routes/toolsOperations.route.js";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
+// import cookieParser from "cookie-parser";
 import compression from "compression";
 import helmet from "helmet";
 
@@ -20,7 +20,7 @@ app.use(compression()); // Gzip compression
 app.use(helmet()); // Secure HTTP headers
 app.use(express.json());
 app.use(cors());
-app.use(cookieParser());
+// app.use(cookieParser());
 
 // api checker
 app.get("/", (req, res) => {
